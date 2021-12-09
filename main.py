8411e57a5b90ee5d3914f45669fd31861f08f182
Marshall <91077783+marshallc03@users.noreply.github.com>
2021-12-09 02:44:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from replit import clear
+import art
+#HINT: You can call clear() to clear the output in the console.
+
+print(art.logo)
+
+def calculate_winner(bids_dict):
+  winner = ''
+  winning_bid = 0
+  for key, value in bids_dict.items():
+    if value > winning_bid:
+      winning_bid = value
+      winner = key
+  print(f"The winner is {winner} with a bid of ${winning_bid}")
+
+more_bids = True
+all_bids = {}
+
+while more_bids:
+  name = input("Bidder name:\n")
+  bid = float(input("Bid amount: \n$"))
+  repeat = input("Are there more bidders? Enter 'y' or 'n': \n")
+
+  all_bids[name] = bid
+  if repeat.lower() == 'n':
+    more_bids = False
+    calculate_winner(all_bids)
+  else:
+    clear()
